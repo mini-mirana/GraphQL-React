7b5d412d096c87ee4a69b5a146f812febaacfde6
mini-mirana <mini_mirana@yahoo.com>
2022-05-15 21:12:01 +0200

    feat: :sparkles: Add access to users' company in the GraphQL schema

--- 01-users/schema/schema.js
+++ 01-users/schema/schema.js
@@ -7,12 +7,27 @@ const {
     GraphQLSchema
 } = graphql
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+})
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString},
         firstName: { type: GraphQLString},
-        age: { type: GraphQLInt}
+        age: { type: GraphQLInt},
+        comapny: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then(resp => resp.data)
+            }
+        }
     }
 })
 
